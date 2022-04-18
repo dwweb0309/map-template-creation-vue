bc1904eeb666318258ee01c3ccbf7aa59ec8a5cb
dwweb0309 <dwweb0309@gmail.com>
2022-04-18 12:30:45 +0200

    bootstrap and nuxt

---
+++ components/layouts/TheHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+      <b-container>
+        <b-navbar-brand to="/">
+          Map Template
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse" />
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-navbar-nav v-if="!isLoggedIn" class="ml-auto">
+              <b-nav-item to="/login">
+                Login
+              </b-nav-item>
+              <b-nav-item to="/register">
+                Register
+              </b-nav-item>
+            </b-navbar-nav>
+            <template v-else>
+              <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template #button-content>
+                  <b-icon-person></b-icon-person>
+                </template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item @click="logout">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </template>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import { BIcon, BIconPerson } from 'bootstrap-vue'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  components: {
+    BIcon,
+    BIconPerson
+  },
+  computed: {
+    ...mapGetters('auth', ['isLoggedIn'])
+  },
+  methods: {
+    ...mapActions('auth', ['logout'])
+  }
+}
+</script>
